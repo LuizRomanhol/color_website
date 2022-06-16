cd5b65151ee3345ad245cd7c46e1c79f52e70aa5
LuizRomanhol <48190640+LuizRomanhol@users.noreply.github.com>
2022-06-15 21:49:28 -0300

    Update color_calibration.py

--- src/color_calibration.py
+++ src/color_calibration.py
@@ -1,135 +1,239 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import StringProperty
 import cv2
-import glob
+import matplotlib.pyplot as plt
 import numpy as np
-import imutils
 
-def get_region_mean(img,point,radius):
-    x, y = point
-    samples = img[y-radius:y+radius, x-radius:x+radius]
-    (rsquare, gsquare,bsquare) = cv2.split(samples)
-    r, g, b = [int(rsquare.mean()),int(gsquare.mean()),int(bsquare.mean())]
-    return (r,g,b)
+def hullarea(pts):
+    lines = np.hstack([pts,np.roll(pts,-1,axis=0)])
+    area = 0.5*abs(sum(x1*y2-x2*y1 for x1,y1,x2,y2 in lines))
+    return area
 
-def white_balance(ref,img,x,y,radius):
+def remove_one_vertex(hull):
+    new_hull = []
+    values = {}
+    for i in range(len(hull)):
+        test = []
+        for j in range(len(hull)):
+            if j != i:
+                test.append(hull[j])
+        values[hullarea(test)] = test
+    return np.array(values[max(list(values.keys()))])
+
+def quality_control(hull,thresh=0.75):
+    dists = []
+    for i in hull:
+        for j in hull:
+             dists.append(np.linalg.norm(i-j))
+    a = np.mean(dists)
+    c = (a*a)/hullarea(hull)
+
+    if (c > thresh):
+        return None
+
+    if a < 16:
+        return None
+    return hull
+
+def quadrilateralize(hull):
+    if len(hull) < 4:
+        return None
+    else:
+        while(len(hull) != 4):
+            hull = remove_one_vertex(hull)
+    return quality_control(hull)
+
+def get_rects(img,use_canny=True):
+
+    if use_canny:
+        img = cv2.Canny(img,32,32)
+        cv2.imwrite("canny.jpg",img)
+        img = cv2.imread("canny.jpg")
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
+    cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    paint_img = img.copy()
+    rects = []
+
+    for i in range(len(cnts)):
+        hull = cv2.convexHull(cnts[i])
+        new_hull = []
+        for i in hull:
+            new_hull.append(i[0])
+
+        hull = quadrilateralize(new_hull)
+
+        if hull is not None:
+
+            #cv2.drawContours(paint_img, np.array([hull]), -1, (0, 0, 255), 2)
+            rects.append(hull)
+
+    #cv2.imwrite("salvo.png",paint_img)
+    #plt.imshow(paint_img)
+    return rects
+
+def filter(rects,img):
+    paint_img = img.copy()
+
+    areas = []
+    for rect in rects:
+        areas.append(hullarea(rect))
+    mode = sorted(areas)[int(len(areas)/2)]
+    new_rects = []
+    for rect in rects:
+        t = 0.5
+        area = hullarea(rect)
+        if ((area/mode < 1/t) and (area/mode > t)):
+            new_rects.append(rect)
+            #cv2.drawContours(paint_img, np.array([rect]), -1, (0, 255, 0), 2)
+    #plt.imshow(paint_img)
+    return new_rects
+
+def get_pallete_box(rects,img):
+    paint_img = img.copy()
+
+    points = []
+
+    for rect in rects:
+        for point in rect:
+            points.append(point)
+
+    cnt = cv2.convexHull(np.array(points))
+
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    #cv2.drawContours(paint_img,[box],0,(0,255,0),5)
+
+    #plt.imshow(paint_img)
+    return box
+
+def organize(a):
+    a = a[a[:, 0].argsort()]
+    a = a[:, a[0, :].argsort()]
+    return a
+
+def homography(rect,img):
+
+    #rect = organize(rect)
+    size = 128
+    width = size
+    height = size
+
+    srcpts = np.float32(rect)
+    destpts = np.float32([[0, 0], [0, height], [width, height], [width, 0]])
+    resmatrix = cv2.getPerspectiveTransform(srcpts, destpts)
+    resultimage = cv2.warpPerspective(img, resmatrix, (width, height))
+
+    plt.imshow(resultimage)
+    #cv2.imwrite("debug_pallete.jpg",resultimage)
+    return resultimage
+
+def get_palette(img):
+    rects = get_rects(img)
+    rects = filter(rects,img)
+    rect = get_pallete_box(rects,img)
+    palette = homography(rect,img)
+    return palette, rect
+
+def resize(img):
+    maxsize = 640
+    img = cv2.resize(img, (maxsize,int(img.shape[0]*maxsize/img.shape[1])))
+    return img
+
+
+def get_color_points(img):
+    img = resize(img)
+    pallete, rect = get_palette(img)
+
+    colors = [[10,10,10],[20,20,20],[10,10,10]]
+    gray = []
+    for i in colors:
+        gray.append(int(np.mean(i)))
+
+    cont = 0
+    while ((gray != sorted(gray)) and cont < 4):
+        pallete = cv2.rotate(pallete, cv2.ROTATE_90_CLOCKWISE)
+        points = []
+        for i in range(1,7):
+            x = int(pallete.shape[1]/6*(i-0.5))
+            y = int(pallete.shape[0]/6*5.25)
+            points.append([x,y])
+
+        colors = []
+        for i in points:
+            colors.append(pallete[i[1]][i[0]][:])
+
+        gray = []
+        for i in colors:
+            gray.append(int(np.mean(i)))
+        cont = cont + 1
+
+    #cv2.imwrite("pallete.jpg",pallete)
+    return np.array(colors), rect
+
+def white_balance(img,pallete_colors):
 
     total = np.array([0,0,0])
     arr = [2,3]
+
     for i in arr:
-        total = total + get_region_mean(ref,(x[i],y[i]),radius)
-    
+        total = total + pallete_colors[i]
+
+    #grey = [((total/len(x)).mean())]*3
     grey = [((total/len(arr)).mean())]*3
     delta = (grey - total/len(arr)).astype(int)
-    
+
     return np.clip(img.astype(int) + delta,0,255).astype('uint8')
 
-def intensity_adjust(ref,img,x,y,radius):
-    graytones = [243,200,161,120,85,52]
+def intensity_adjust(img,pallete_colors):
     gray = []
-    print(x,graytones,y)
 
-    for i in range(len(x)):
-        gray.append(int(np.array(get_region_mean(ref,(x[i],y[i]),radius)).mean()))
-    
+    graytones = [52,85,120,161,200,243]
+
+    for i in range(len(pallete_colors)):
+        gray.append(int(np.array(pallete_colors[i]).mean()))
+
     z = np.polyfit(gray,graytones,3)
     p = np.poly1d(z)
 
     grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     pgray = np.clip(p(grayimg),0,255)
     delta = np.clip(pgray.astype(int) - grayimg.astype(int) + np.ones(grayimg.shape)*255,0,255*2)
-    deltacolor = cv2.cvtColor(delta.astype('uint16'), cv2.COLOR_GRAY2RGB) 
+    deltacolor = cv2.cvtColor(delta.astype('uint16'), cv2.COLOR_GRAY2RGB)
     finalimg = np.clip(img.astype(int) + deltacolor.astype(int) - (np.ones(img.shape)*255).astype(int),0,255)
 
     return finalimg.astype('uint8')
-    
-def get_coords(image_path, template_path, template_coords,show = False):
-    template = cv2.imread(template_path)
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    template = cv2.Canny(template, 50, 200)
-    (tH, tW) = template.shape[:2]
-    if show:
-        cv2_imshow(template)
 
-    for imagePath in glob.glob(image_path):
 
-        image = cv2.imread(imagePath)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        found = None
+def debug(img,rect):
+    img = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_GRAY2RGB)
+    debug_img = resize(img.copy())
+    debug_img = cv2.drawContours(debug_img, np.array([rect]), -1, (167,207,120), 12)
+    return debug_img
 
-        for scale in np.linspace(0.025, 0.5, 20)[::-1]:
+def calibrate_batch(ref,imgs):
 
-            resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
-            r = gray.shape[1] / float(resized.shape[1])
+    results = []
+    samples, rect = get_color_points(ref)
+    print("PRINT DOS SAMPLES",samples)
 
-            if resized.shape[0] < tH or resized.shape[1] < tW:
-                break
+    debug_img = debug(ref,rect)
+    #cv2.imwrite("debug_find_pallete.jpg",debug_img)
 
-            edged = cv2.Canny(resized, 50, 200)
-            result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-            (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+    for i in imgs:
+        img = i.copy()
+        img = white_balance(img,samples)
+        img = intensity_adjust(img,samples)
+        results.append(img)
 
-            if True:
+    return results, debug_img
 
-                clone = np.dstack([edged, edged, edged])
-                cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-                  (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-                if show:    
-                    cv2_imshow(clone)
-                    cv2.waitKey(0)
-
-            if found is None or maxVal > found[0]:
-                found = (maxVal, maxLoc, r)
-
-        (_, maxLoc, r) = found
-        (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-        (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-
-        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-
-        if show:
-            cv2_imshow(image)
-        cv2.waitKey(0)
-
-    path = image_path
-    image = cv2.imread(path)
-    radius = int(image.shape[0]*0.075)
-    color = (255, 0, 0)
-    thickness = int(image.shape[0]*0.01)
-    pallete_coords = []
-
-    for coord in template_coords:
-        x = int(coord[0]/152 * (endX - startX))
-        y = int(coord[1]/101 * (endY - startY))
-        x = x + startX
-        y = y + startY
-        coord = (x,y)
-        if show:
-            image = cv2.circle(image, coord, radius, color, thickness)
-        pallete_coords.append((x,y))
-
-    if show:
-        cv2_imshow(image)
-
-    return pallete_coords
- 	
-def calibrate(ref,imgs,pallete_coords,radius=5):
-	x = []
-	y = []
-	for xy in pallete_coords:
-		x.append(xy[0])
-		y.append(xy[1])
-	for i in range(len(imgs)):
-		imgs[i] = white_balance(ref,imgs[i],x,y,radius)
-		imgs[i] = intensity_adjust(ref,imgs[i],x,y,radius)
-	return imgs
- 	
-def draw_circles(img, coords):
-	thickness = int(img.shape[0]*0.025)
-	for coord in coords:
-		img = cv2.circle(img, coord, radius=0, color=(0,255,255), thickness=thickness)        
-	return img
-	
+#def calibrate(img):
+#    cv2.imwrite("imagem_de_debug_dia_14_antes.jpg",img)
+#    samples, rect = get_color_points(img)
+#    img = white_balance(img,samples)
+#    img = intensity_adjust(img,samples)
+#    cv2.imwrite("imagem_de_debug_dia_14_depois.jpg",img)
+#    return img, rect
